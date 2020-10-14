12ed09e387f589cdbf2cf85538da26500a18fa45
aliciamaravat <alicia82@orange.fr>
2020-10-14 20:09:31 +0200

    add rigidbody on many objects to be able to take them + writing script to use key on the chest

---
+++ Assets/Scripts/OpenBox.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+using Valve.VR.InteractionSystem;
+
+public class OpenBox : MonoBehaviour
+{
+    public float smooth = 2.0f;
+    public float BoxOpenAngle = -60.0f;
+
+    private Vector3 defaultRot;
+    private Vector3 openRot;
+    private bool unlock = false;
+    private bool open = false;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        defaultRot = transform.eulerAngles;
+        openRot = new Vector3(defaultRot.x, defaultRot.y, defaultRot.z - BoxOpenAngle);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (unlock)
+        {
+            defaultRot = Vector3.Lerp(defaultRot, openRot, Time.deltaTime);
+            transform.eulerAngles = defaultRot;
+        }
+        
+
+        if (this.GetComponent<Interactable>().isHovering)
+        {
+            open = true;
+        }
+        else
+        {
+            open = false;
+        }
+
+        if (GameObject.FindWithTag("Key").GetComponent<keyBehaviour>().isUsed && open)   //peut s'ouvrir si on a la clé en main
+        {
+            unlock = !unlock;
+        }
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.tag == "Player")    //ouvrir le coffre en le touchant 
+        {
+            open = true;
+        }
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        if (col.tag == "Player")
+        {
+            open = false;
+        }
+    }
+}
---
+++ Assets/Scripts/keyBehaviour.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
+using UnityEngine.UI;
+
+public class keyBehaviour : MonoBehaviour
+{
+
+    public bool isHeld;  //si le smartphone est tenu dans la main du joueur ou pas
+    public bool isUsed;  //si la clé a touché le couvercle du coffre
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isHeld = false;
+        isUsed = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isHeld = GetComponent<Throwable>().GetAttached();
+
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.tag == "Lock")
+        {
+            isUsed = true;
+        }
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        
+    }
+}
