9fccab348714b0ddf7aacd4f53c2660c8824c968
aliciamaravat <alicia82@orange.fr>
2020-10-20 20:46:10 +0200

    Changing materials + Walls modified into Solid walls + Removing clock + Adding Switchs to on/off lights and a text visible only when light is off to create an other enigma

---
+++ Assets/Scripts/OnOffLight.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnOffLight : MonoBehaviour
+{
+    public bool lightOff = false;
+    Light thisLight;
+    Renderer thisRenderer;
+    public Material onMaterial;
+    public Material offMaterial;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        thisLight = gameObject.GetComponent<Light>();
+        thisRenderer = gameObject.GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (lightOff)
+        {
+            thisLight.enabled = false;
+            thisRenderer.material = offMaterial;
+        }
+        else
+        {
+            thisLight.enabled = true;
+            thisRenderer.material = onMaterial;
+        }
+    }
+}
---
+++ Assets/Scripts/SwitchScript.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class SwitchScript : MonoBehaviour
+{
+    // Moving switch variables
+    public float switchAngle = 180.0f;
+    private Vector3 lightRot;
+    private Vector3 nightRot;
+    private bool changeSwitch = false;
+
+    // Nightmode variables
+    public bool nightMode = false;
+    private bool changeLight = false;
+    public GameObject linkedLight;
+    public GameObject nightNotes;
+
+    void Start()
+    {
+        lightRot = transform.localEulerAngles;
+        nightRot = new Vector3(lightRot.x, lightRot.y + switchAngle, lightRot.z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (changeSwitch)
+        {
+            transform.localEulerAngles = nightRot;
+            nightMode = true;
+        }
+        else
+        {
+            transform.localEulerAngles = lightRot;
+            nightMode = false;
+        }
+
+        if(changeLight)
+        {
+            changeLight = false;
+            linkedLight.GetComponent<OnOffLight>().lightOff = nightMode;
+            nightNotes.SetActive(nightMode);
+        }
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.tag == "Player")    
+        {
+            changeSwitch = !changeSwitch;
+            changeLight = true;
+        }
+    }
+
+    // Debug with mouse
+    void OnMouseDown()
+    {
+        changeSwitch = !changeSwitch;
+        changeLight = true;
+    }
+
+}
